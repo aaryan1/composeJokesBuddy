42466a554d63cfaf093ab7c86f2af8244f97dcb1
Takendra saraswat <aaryan224@gmail.com>
2022-03-21 14:14:37 +0530

    Profile.kt added.

--- app/src/main/java/com/tks/jokesbuddy/Pages/ProfilePage.kt
+++ app/src/main/java/com/tks/jokesbuddy/Pages/ProfilePage.kt
@@ -1,12 +1,16 @@
 package com.tks.jokesbuddy.Pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tks.jokesbuddy.ui.theme.BackgroundColor
+import org.intellij.lang.annotations.JdkConstants
 
 
 @Composable
@@ -14,7 +18,11 @@ fun profilePage()
 {
       Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.BackgroundColor) {
 
+       Column(Modifier.fillMaxSize(),
+           verticalArrangement = Arrangement.Center,
+           horizontalAlignment = Alignment.CenterHorizontally) {
 
+       }
       }
 }
 @Preview(showBackground = true)
